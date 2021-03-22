72a621ab4e9165a8516c9223c088a267b213e690
Filippo Barcellos <barcellos.filippo@gmail.com>
2021-03-22 12:55:46 +0000

    added solutions to challenges

---
+++ movies_app/src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  );
+}
